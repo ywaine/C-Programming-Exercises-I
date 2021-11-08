9fd27f538c82caf9feebdfa060116f487f11676c
Antony rubombora <tonyrubombora@gmail.com>
2021-11-08 15:45:34 +0300

    Solution ex 1 & ex

--- ex_2.c
+++ ex_2.c
@@ -2,7 +2,13 @@
 
 int main()
 {
-    printf("Hello World");
-
-    return 0;
+	int i;
+	for (i = 1; i < 100; i++) {
+		if (i % 3 == 0) {
+			printf("%d\n", i);
+		}
+		if (i % 4 == 0) {
+			printf("%d\n", i);
+		}
+	}
 }
