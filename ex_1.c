b006266fa0e72a0b08d8d2f34d477acfee2f077b
Seong-joo <93765136+Seong-joo@users.noreply.github.com>
2021-11-09 23:45:53 +0100

    Ex1.1 (Joo)

--- ex_1.c
+++ ex_1.c
@@ -1,8 +1,13 @@
+/*Print even no btw 0 to 100*/
+
 #include <stdio.h>
 
 int main()
 {
-    printf("Hello World");
-
-    return 0;
+	for (int z = 0;z <= 100; z++) {
+		if (z % 2 == 0) {
+			printf("%d\n", z);
+		}
+	}
+	return 0;
 }
