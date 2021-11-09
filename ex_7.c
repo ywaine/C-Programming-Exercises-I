2fe1a459ae6cbce310e472a387eb33230757b320
Antony rubombora <tonyrubombora@gmail.com>
2021-11-09 11:47:44 +0300

    exercise 7 with mod

--- ex_7.c
+++ ex_7.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
-
-int main()
+//void method returns no value
+void main()
 {
-    printf("Hello World");
+    int i;
+    for (i = 0; i < 100; i++) {
+        //conditional: if i/2 has a reminder it is a prime number
+        if (i % 2 > 0) {
+            
+            printf("%d\n", i);
 
-    return 0;
+        }
+    }
+
 }
