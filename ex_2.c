5552dc947cf0bdc8404e99e1741cd3e483731342
Seong-joo <93765136+Seong-joo@users.noreply.github.com>
2021-11-10 10:01:49 +0100

    Ex1.2 (Joo)Ex1.1 (Joo)

--- ex_2.c
+++ ex_2.c
@@ -1,8 +1,13 @@
+/*Print common multiples of 3 and 4 btw 0 to 100*/
 #include <stdio.h>
 
 int main()
 {
-    printf("Hello World");
-
-    return 0;
+	for (int z = 0;z <= 100; z++) {
+		if (z % 12 == 0) {
+			printf("%d\n", z);
+		}
+	}
+	return 0;
 }
+
--- ex_4.c
+++ ex_4.c
@@ -1,8 +1,14 @@
+/*4. Print 7 times table*/
 #include <stdio.h>
 
 int main()
 {
-    printf("Hello World");
-
-    return 0;
+	int a, b, c;
+	a = 1;
+	c = 7 * a;
+	for (int a = 1;a < 10; a++) {
+		c = 7 * a;
+		printf("7 * %d = %d\n", a, c);
+	}
 }
+
