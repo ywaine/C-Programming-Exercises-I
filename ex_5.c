4be12672e9f62518f41e2a991d2a9b00fa73b5ba
Seong-joo <93765136+Seong-joo@users.noreply.github.com>
2021-11-18 21:51:02 +0100

    joo

--- ex_5.c
+++ ex_5.c
@@ -1,8 +1,25 @@
+/* 5. Find "a" which is 5 digit number (10,000-99,999) and (100,000+a)*3 = (a*10)+1 */
+
 #include <stdio.h>
+#include <string>
+
+unsigned concatenate(unsigned x, unsigned y) {
+	unsigned pow = 10;
+	while (y >= pow)
+		pow *= 10;
+	return x * pow + y;
+}
 
 int main()
 {
-    printf("Hello World");
+	for (int a = 10000, b = 0;a < 99999; a++) {
+		int d = a * 3; //3 times of a
+		if (concatenate(a, 1) == concatenate(1, a) * 3)
+		{
+			printf("a is ", a);
+			printf("%d", a);
 
-    return 0;
+		}
+
+	}
 }
--- ex_8.c
+++ ex_8.c
@@ -1,8 +1,19 @@
+/*8. Print only the 20 smallest prime numbers*/
 #include <stdio.h>
-
-int main()
+void main()
 {
-    printf("Hello World");
+	for (int i = 1, c = 0 ; i < 100; i++)
+	{
+		//int c = 0;
 
-    return 0;
-}
+		for (int j = 2; j <= i; j++)   // Changed upper bound
+		{
+			if (i == j && c < 20) {  // Changed condition and reversed order of if:s
+				c = c + 1;
+				printf("%d\n", i);
+			}
+			else if (i % j == 0) {
+				//printf("");
+				break;
+			}
+
