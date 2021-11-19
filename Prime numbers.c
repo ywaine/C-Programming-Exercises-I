9e8bb61b37cf97835b536a89b1892a02f06657a3
Nikita <93765124+PopadosNikitos@users.noreply.github.com>
2021-11-19 10:00:49 +0300

    Add files via upload

---
+++ Prime numbers.c
@@ -0,0 +1,36 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+void intprime( int i ){
+for (int i = 1; i <= 100; i++){
+    for (int summ = 0 ;summ <= 2; summ++){
+        printf("%d, is a prime number\n");
+    }
+}
+    if (i % 2 == 0){
+        summ ++
+    }
+    else if (i % 3 == 0){
+        summ ++
+    }
+    else if (i % 5 == 0){
+        summ ++
+    }
+    else if (i % 7== 0){
+        summ ++
+    }
+}
+int main()
+{
+intprime(i);
+int i = 0
+int summ = 0
+return 0;
+}
+
